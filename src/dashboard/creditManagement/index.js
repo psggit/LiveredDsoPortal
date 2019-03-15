b75cf1998d3d50142d410c2a13a063c28294bdba
Aparna Chinnaiah <aparna.chinnaiah@hipbar.com>
2019-03-15 17:42:09 +0530

    strated with credit mgmt and style for card

---
+++ src/dashboard/creditManagement/index.js
@@ -0,0 +1,18 @@
+import React from "react"
+import PageHeader from "Components/pageheader"
+
+class CreditManagement extends React.Component {
+  constructor() {
+    super()
+  }
+
+  render() {
+    return (
+      <div id="CreditManagement">
+        <PageHeader pageName="Credit Management" />
+      </div>
+    )
+  }
+}
+
+export default CreditManagement
