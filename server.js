d010e2d2e2849d42e269f7d347fda04bb2be29d4
AparnaRada <50664973+AparnaRada@users.noreply.github.com>
2019-10-04 19:55:18 +0530

    Infra migration (#3)
    * Debug
    
    * Debug

--- server.js
+++ server.js
@@ -39,5 +39,5 @@ app.get('/*', (req, res) => {
     }
   })
 })
-app.listen(process.argv[2] || 8080)
-console.log('Server is running on port 8080')
+app.listen(process.argv[2] || 8004)
+console.log('Server is running on the port 8080')
